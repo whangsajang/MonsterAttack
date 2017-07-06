5a0e0661d4df79dd8e0c42aeafd14cce0e50bd61
David Whang <whangsajang@gmail.com>
2017-07-05 18:17:37 -0700

    check Win

--- app.js
+++ app.js
@@ -4,5 +4,52 @@ new Vue ({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
+    },
+    methods: {
+        startGame: function(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.gameIsRunning = true;
+        },
+        attack: function(){
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if(this.checkWin()){
+                return;
+            }
+
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function(){
+
+        },
+        heal: function(){
+
+        },
+        giveUp: function(){
+
+        },
+        calculateDamage: function(min, max){
+            return Math.max(Math.floor(Math.random() * max) + 1, min)
+        },
+        checkWin: function(){
+            if(this.monsterHealth <= 0){
+                if(confirm('You\'ve Won! New Game?')){
+                    this.startGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if(this.playerHealth <= 0){
+                if(confirm('You\'ve Lost New Game?')){
+                    this.startGame();
+                } else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+
     }
 })
